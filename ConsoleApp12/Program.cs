4bd1cddd53dc5bea73aad749e308288042f145bc
Alexey Kapp <alexeikapp@yandex.ru>
2023-12-13 14:56:59 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Serilog;
+using Serilog.Events;
+using System.IO;
+using System.Text.RegularExpressions;
+using ConsoleApp12;
+
+class Program
+{
+    static List<string> existingLogins = new List<string> { "user1", "user2", "user3", "user4", "user5" };
+
+    static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.Console()
+            .WriteTo.File("registration.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+        Console.WriteLine("Введите логин:");
+        string login = Console.ReadLine();
+        Console.WriteLine("Введите пароль:");
+        string password = Console.ReadLine();
+        Console.WriteLine("Подтвердите пароль:");
+        string confirmPassword = Console.ReadLine();
+        Registration registration = new Registration();
+        (bool registrationSuccess, string errorMessage) = registration.ValidateUserRegistration(login, password, confirmPassword);
+
+        if (registrationSuccess)
+        {
+            LogInformation(login, password, confirmPassword);
+            Console.WriteLine("Успешная регистрация.");
+        }
+        else
+        {
+            LogError(login, password, confirmPassword, errorMessage);
+            Console.WriteLine($"Ошибка регистрации: {errorMessage}");
+        }
+        Log.CloseAndFlush();
+    }
+    static void LogInformation(string login, string password, string confirmPassword)
+    {
+        Log.Information("{DateTime}: Успешная регистрация - Логин: {Login}, Пароль: {Password}, Подтверждение: {ConfirmPassword}",
+            DateTime.Now, login, MaskPassword(password), MaskPassword(confirmPassword));
+    }
+
+    static void LogError(string login, string password, string confirmPassword, string errorMessage)
+    {
+        Log.Error(
+            "{DateTime}: Неуспешная регистрация - Логин: {Login}, Пароль: {Password}, Подтверждение: {ConfirmPassword}, Ошибка: {ErrorMessage}",
+            DateTime.Now, login, MaskPassword(password), MaskPassword(confirmPassword), errorMessage);
+    }
+    static string MaskPassword(string password)
+    {
+        return new string('*', password.Length);
+    }
+}
---
+++ ConsoleApp12/Registration.cs
@@ -0,0 +1,121 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    public class Registration
+    {
+        static List<string> existingLogins = new List<string> { "user1", "user2", "user3", "user4", "user5" };
+
+        public (bool, string) ValidateUserRegistration(string login, string password, string confirmPassword)
+        {
+            if (string.IsNullOrEmpty(login))
+            {
+                Console.WriteLine("Пустая строка в качестве логина");
+                return (false, "Пустая строка в качестве логина");
+            }
+            else if (string.IsNullOrEmpty(password))
+            {
+                Console.WriteLine("Пустая строка в качестве пароля");
+                return (false, "Пустая строка в качестве пароля");
+            }
+            else if (string.IsNullOrEmpty(confirmPassword))
+            {
+                Console.WriteLine("Пустая строка в качестве подтверждения пароля");
+                return (false, "Пустая строка в качестве подтверждения пароля");
+            }
+            if (existingLogins.Contains(login))
+            {
+                Console.WriteLine("Логин уже существует");
+                return (false, "Логин уже существует");
+            }
+            if (login.Length < 5)
+            {
+                return (false, "Логин слишком короткий");
+            }
+
+            bool isValidLogin = Regex.IsMatch(login, @"^(?:\+\d{1,3}-)?\d{1,4}-\d{1,4}-\d{1,4}$|^\w+@\w+\.\w+$");
+            if (!isValidLogin)
+            {
+                return (false, "Логин не соответствует формату телефона или электронной почты");
+            }
+
+            if (password != confirmPassword)
+            {
+                return (false, "Пароль и подтверждение пароля не совпадают.");
+            }
+
+            if (password.Length < 7)
+            {
+                return (false, "Пароль слишком короткий.");
+            }
+
+            if (!IsCyrillic(password))
+            {
+                return (false, "Пароль должен содержать только кириллические символы.");
+            }
+
+            bool hasLower = false;
+            bool hasUpper = false;
+            bool hasDigit = false;
+            bool hasSpecialChar = false;
+
+            foreach (char c in password)
+            {
+                if (char.IsLower(c))
+                {
+                    hasLower = true;
+                }
+                else if (char.IsUpper(c))
+                {
+                    hasUpper = true;
+                }
+                else if (char.IsDigit(c))
+                {
+                    hasDigit = true;
+                }
+                else if (char.IsSymbol(c) || char.IsPunctuation(c))
+                {
+                    hasSpecialChar = true;
+                }
+            }
+
+            if (!hasLower)
+            {
+                return (false, "Пароль не содержит буквы в нижнем регистре.");
+            }
+
+            if (!hasUpper)
+            {
+                return (false, "Пароль не содержит буквы в верхнем регистре.");
+            }
+
+            if (!hasDigit)
+            {
+                return (false, "Пароль не содержит цифры.");
+            }
+
+            if (!hasSpecialChar)
+            {
+                return (false, "Пароль не содержит спецсимвола.");
+            }
+
+            return (true, "Успешно");
+        }
+
+        static bool IsCyrillic(string password)
+        {
+            return !Regex.IsMatch(password, @"[a-zA-Z]");
+        }
+
+        static bool IsCyrillic(char c)
+        {
+            return (c >= 'А' && c <= 'я') || (c >= 'ё' && c <= 'ё');
+        }
+    }
+}
